830b3f834b3197d3998cec58583d28ed47b87db5
NasiphiMjobo <u18074074@tuks.co.za>
2023-04-18 16:51:31 +0200

    Initial Commit

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,39 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore; 
+
+
+namespace API.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]  // GET api/users 
+
+	public class UsersController : ControllerBase
+	{
+        private readonly DataContext _context; 
+        
+        public UsersController(DataContext context)
+        {
+            _context = context; 
+        }
+
+        //endpoints need HTTP method to make requests & to get from an API endpoint 
+        // you can use HTTP GET method 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()  //action result can return http responses such as  Not Found , asynchronous so that we can make multithreaded 
+        {
+            var users = await _context.Users.ToListAsync(); // gets a list of users from the database 
+            return users; 
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+                                                            //finding user with that specific id 
+            return await _context.Users.FindAsync(id);
+        }
+
+
+    }
+}
